ac39b11244c0c524cf864fa575a79bf38f7f2b6d
tienc0322h1 <navicomj400s@gmail.com>
2022-06-02 16:36:13 +0700

    2.6.22l5

---
+++ Web_Backend_Development_with_NodeJS/Practive3/HTTP-SERVER/server.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+const qs = require('qs');
+const server = http.createServer(function (req, res) {
+    if (req.method ===  'GET') {
+        fs.readFile('./views/register.html', function (err, data) {
+            res.writeHead(200, {'Contain-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+        })
+    }
+    else {
+        let data = '';
+        req.on('data', chunk => {
+            data += chunk;
+        })
+        req.on('end', () => {
+            const userInfo = qs.parse (data);
+            fs.readFile('./views/info.html', 'utf-8', function (error, datahtml) {
+                if (error) {
+                    console.log(error);
+                }
+                datahtml = datahtml.replace('{name}', userInfo.name);
+                datahtml = datahtml.replace('{email}', userInfo.email);
+                datahtml = datahtml.replace('{password}', userInfo.password);
+                res.writeHead(200, {'Contain-Type': 'text/html'});
+                res.write(datahtml);
+                return res.end();
+            })
+        })
+        req.on('error', () => {
+            console.log('error')
+        })
+    }
+} )
+server.listen(8080, function () {
+    console.log('Server running at localhost:8080')
+})
